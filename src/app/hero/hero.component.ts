926ed71d7cc3d06fae9aeced9c8a36296c94106d
AmalSkumar <amal.sasikumar@hrblock.com>
2021-07-10 20:50:29 +0530

    Angular 10 upgrade

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
 @Component({
   selector: "app-hero",
   templateUrl: "./hero.component.html",
