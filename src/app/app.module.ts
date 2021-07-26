29a5b4e1ed87cb84b2a58501eb074ce4f18829af
amalskumar <focusonamal@gmail.com>
2021-07-26 16:36:36 +0530

    included routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,  ],
+    AppComponent,
+    HomeComponent,
+    ClientsComponent,
+    ServicesComponent,
+    AboutusComponent,
+    BlogComponent,
+    ContactComponent,  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'clients', component: ClientsComponent },
+      { path: 'services', component: ServicesComponent },
+      { path: 'about', component: AboutusComponent },
+      { path: 'blogs', component: BlogComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: '', redirectTo: '', pathMatch: 'full' }, 
+      { path: '**', component: HomeComponent }
+   ])
   ],
   providers: [],
   bootstrap: [AppComponent]
