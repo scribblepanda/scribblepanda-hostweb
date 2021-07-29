79be76142de13bf93095def10f20a0ffb88e4459
amalSkumar <focusonamal@gmail.com>
2021-07-29 12:57:08 +0530

    added blog

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
-  selector: 'app-blog',
-  templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  selector: "app-blog",
+  templateUrl: "./blog.component.html",
+  styleUrls: ["./blog.component.scss"],
 })
 export class BlogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  items = ["", "", "", "", ""];
+  constructor() {}
+  quote = "Blogging is a conversation, not a code.";
+  heading = "Blog";
+  ngOnInit(): void {}
 }
