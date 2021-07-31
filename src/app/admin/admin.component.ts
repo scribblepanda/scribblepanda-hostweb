a0ad0350066dbcd6815b1322baf98fdf2b6836e2
amalSkumar <focusonamal@gmail.com>
2021-07-31 09:41:42 +0530

    form

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/auth";
 import firebase from "firebase/app";
+import { AngularFireStorage } from "@angular/fire/storage";
+import { AngularFireModule } from "@angular/fire";
+import { finalize } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-admin",
@@ -10,8 +15,23 @@ import firebase from "firebase/app";
 export class AdminComponent implements OnInit {
   error: any;
   content = "";
+  downloadURL: Observable<string>;
 
-  constructor(public auth: AngularFireAuth) {}
+  constructor(
+    public auth: AngularFireAuth,
+    private storage: AngularFireStorage
+  ) {}
+  uploadFile(event) {
+    const file = event.target.files[0];
+    const filePath = Date().toString();
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+    task
+      .snapshotChanges()
+      .pipe(finalize(() => (this.downloadURL = fileRef.getDownloadURL())))
+      .subscribe();
+  }
+
   login() {
     this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
   }
