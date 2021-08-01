63634bb50a0bd6400ede61e4b0eefbfe71c1b4e6
AmalSkumar <amal.sasikumar@hrblock.com>
2021-08-01 23:27:29 +0530

    Ui updates-amal

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: "AIzaSyDnzU9--oxATv1swhs7d0dn54KeLVnE3eI",
+    authDomain: "scribblepanda-ce168.firebaseapp.com",
+    databaseURL:
+      "https://scribblepanda-ce168-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "scribblepanda-ce168",
+    storageBucket: "scribblepanda-ce168.appspot.com",
+    messagingSenderId: "63342501241",
+    appId: "1:63342501241:web:915dd0479200554ecba3a8",
+    measurementId: "G-TMLJ8NEQ80",
+  },
 };
