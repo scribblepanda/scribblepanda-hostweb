2d341355232c3c939541d5d8e307617005197d88
AmalSkumar <amal.sasikumar@hrblock.com>
2021-08-01 23:37:49 +0530

    Ui updates-amal-deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyDnzU9--oxATv1swhs7d0dn54KeLVnE3eI",
     authDomain: "scribblepanda-ce168.firebaseapp.com",
