8dea71de6343dc0bf634aa6f9493d4d209996c32
akshaikrishnan <akshaik143@gmail.com>
2021-08-05 22:49:23 +0530

    about update

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-aboutus',
-  templateUrl: './aboutus.component.html',
-  styleUrls: ['./aboutus.component.scss']
+  selector: "app-aboutus",
+  templateUrl: "./aboutus.component.html",
+  styleUrls: ["./aboutus.component.scss"],
 })
 export class AboutusComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }heading = "About Us"
-  // quote ="Every good Coversations starts with good listening."
+  ngOnInit(): void {}
+  heading = "About Us";
+  quote =
+    "Our Company is built on people - who work for us, and those we do business with.";
 }
