62d79637797c1dbf95a674a67a3b71f4d854edf3
akshaikrishnan <akshaik143@gmail.com>
2021-08-06 22:19:52 +0530

    contact page ui changes

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 @Component({
@@ -16,13 +16,16 @@ export class ContactComponent implements OnInit {
     document.documentElement.scrollTop = 0;
     this.formsubmitted = false;
   }
+  sendButton = "Send Query";
   heading = "Contact Us";
   quote = "Every good Coversations starts with good listening.";
   onSubmit(form: NgForm) {
+    this.sendButton = "Sending...";
     console.log("Your form data : ", form.value);
     if (form.valid) {
       const formValue = form.value;
       const headers = new HttpHeaders({ "Content-Type": "application/json" });
+
       this.http
         .post(
           "https://api.web3forms.com/submit",
@@ -31,8 +34,15 @@ export class ContactComponent implements OnInit {
             name: formValue.name,
             email: formValue.email,
             phone: formValue.phone,
-            service: formValue.service,
+            webDesign: formValue.webDesign,
+            webDevelopment: formValue.webdevelopment,
+            digitalMarketing: formValue.digitalMarketing,
+            mobileApp: formValue.mobileApp,
+            otherService: formValue.others,
+
+            uxSolutions: formValue.uxSolutions,
             budgetRange: formValue.budgetRange,
+            deadline: formValue.deadline,
             textarea: formValue.textarea,
           },
           { headers: headers }
@@ -40,13 +50,19 @@ export class ContactComponent implements OnInit {
         .subscribe((response) => {
           console.log(response);
           this.formsubmitted = true;
-          document.body.scrollTop = 0;
-          document.documentElement.scrollTop = 0;
+          this.scrollView();
+          this.sendButton = "Send Query";
         });
       form.resetForm();
     }
   }
-
+  @ViewChild("contactinfo") MyProp: ElementRef;
+  scrollView() {
+    this.MyProp.nativeElement.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
   bringForm() {
     this.formsubmitted = false;
   }
