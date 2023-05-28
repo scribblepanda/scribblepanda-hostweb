e86e3d313634c542c2846db19027ac1dbe4f9342
Ashabenny123 <71927833+akshaikrishnan@users.noreply.github.com>
2023-05-28 21:12:48 +0530

    init

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,18 +3,9 @@ import { ServerModule } from "@angular/platform-server";
 
 import { AppModule } from "./app.module";
 import { AppComponent } from "./app.component";
-import { TransferState, makeStateKey } from "@angular/platform-browser";
-const MY_DATA_KEY = makeStateKey<string>("myData");
 
 @NgModule({
   imports: [AppModule, ServerModule],
   bootstrap: [AppComponent],
 })
-export class AppServerModule {
-  constructor(private transferState: TransferState) {
-    this.transferState.set(MY_DATA_KEY, "Some data");
-
-    // On the client-side, retrieve the data from TransferState
-    const myData = this.transferState.get(MY_DATA_KEY, "");
-  }
-}
+export class AppServerModule {}
