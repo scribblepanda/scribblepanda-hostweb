9800239a3cdf8103ca38458deefc8c56b444d323
Ashabenny123 <71927833+akshaikrishnan@users.noreply.github.com>
2023-05-28 13:53:01 +0530

    meta-update

--- src/app/blog-details/blog-details.component.ts
+++ src/app/blog-details/blog-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { AngularFirestore } from "@angular/fire/firestore";
 import { Observable } from "rxjs";
 import { Router } from "@angular/router";
-
+import { Meta } from "@angular/platform-browser";
 @Component({
   selector: "app-blog-details",
   templateUrl: "./blog-details.component.html",
@@ -11,15 +11,29 @@ import { Router } from "@angular/router";
 export class BlogDetailsComponent implements OnInit {
   blogg: any;
 
-  constructor(private firestore: AngularFirestore, private route: Router) {
+  constructor(
+    private firestore: AngularFirestore,
+    private route: Router,
+    private meta: Meta
+  ) {
     var postid = this.route.url.split("/");
     console.log(postid[2]);
     firestore
       .collection("post")
       .doc(postid[2])
       .valueChanges()
-      .subscribe((data) => {
+      .subscribe((data: any) => {
         this.blogg = data;
+        console.log(this.blogg);
+        this.meta.addTag({ name: "og:title", content: this.blogg.title });
+        this.meta.updateTag({
+          name: "og:description",
+          content: this.blogg.summary,
+        });
+        this.meta.addTag({
+          name: "og:image",
+          content: data?.cover,
+        });
       });
   }
 
